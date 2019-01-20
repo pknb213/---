0e124c31209f6d46534338f4773531a889822969
YoungJo Cheon <pknb213@naver.com>
2019-01-20 23:04:50 +0900

    2019-01-20 Added bootstrap

---
+++ FlaskServer/views.py
@@ -0,0 +1,56 @@
+from FlaskServer import app
+import os
+import random
+import datetime
+from flask import render_template, request, current_app, make_response, url_for, redirect
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload')
+def ckeditor4():
+    return render_template('ckeditor4.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.route('/ckprocess', methods=['POST'])
+def ckeditor4_process():
+    return redirect('/')
+
+def gen_rnd_filename():
+    filename_prefix = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    return '%s%s' % (filename_prefix, str(random.randrange(1000, 10000)))
+
+@app.route('/ckupload/', methods=['POST', 'OPTIONS'])
+def ckupload():
+    """CKEditor4 file upload"""
+    error = ''
+    url = ''
+    callback = request.args.get("CKEditorFuncNum")
+    if request.method == 'POST' and 'upload' in request.files:
+        fileobj = request.files['upload']
+        fname, fext = os.path.splitext(fileobj.filename)
+        rnd_name = '%s%s' % (gen_rnd_filename(), fext)
+        filepath = os.path.join(current_app.static_folder, 'img', rnd_name)
+        dirname = os.path.dirname(filepath)
+        if not os.path.exists(dirname):
+            try:
+                os.makedirs(dirname)
+            except:
+                error = 'ERROR_CREATE_DIR'
+        elif not os.access(dirname, os.W_OK):
+            error = 'ERROR_DIR_NOT_WRITEABLE'
+        if not error:
+            fileobj.save(filepath)
+            url = url_for('static', filename='%s/%s' % ('img', rnd_name))
+    else:
+        error = 'post error'
+    res = """<script type="text/javascript"> 
+             window.parent.CKEDITOR.tools.callFunction(%s, '%s', '%s');
+             </script>""" % (callback, url, error)
+    response = make_response(res)
+    response.headers["Content-Type"] = "text/html"
+    return response
